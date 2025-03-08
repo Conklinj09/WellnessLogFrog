97db06db0b57d74c41e6d4e8c7a90e4231d72c04
Jeanette Conklin <conklinj09@gmail.com>
2025-03-08 00:02:28 -0800

    typos & update SignUp

--- src/Pages/Sign Up/Sign Up.jsx
+++ src/Pages/Sign Up/Sign Up.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./SignUp.css";
 
 function SignUp() {
     const [email, setEmail] = useState("");
@@ -10,23 +11,26 @@ function SignUp() {
     };
 
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <div className="signup-container">
+            <h2>Create Your Wellness Log Frog Account</h2>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                <input 
+                    type="email" 
+                    placeholder="Email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    required 
                 />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    required 
                 />
                 <button type="submit">Sign Up</button>
             </form>
+            <p>Already have an account? <a href="/login">Login</a></p>
         </div>
     );
 }
