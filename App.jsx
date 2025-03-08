7cb5a34973301f27588c43020e8eb73cbf6bf27c
Jeanette Conklin <conklinj09@gmail.com>
2025-03-07 22:51:22 -0800

    Added App.jsx

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./Pages/home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/auth/Login";
+import SignUp from "./Pages/auth/SignUp";
+
+function App() {
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Contact" element={<Contact />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/SignUp" element={<SignUp />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
